b4f5322c5d034f81b425fb1dc8f375130efdb357
BAGAYOKO OUMAR <bmhaqeel995@gmail.com>
2025-03-30 16:43:09 +0700

    Fix base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/client_oumar_portfolio/',
+  base: '/stylish-omar-portfolio/',
   server: {
     host: "::",
     port: 8080,
